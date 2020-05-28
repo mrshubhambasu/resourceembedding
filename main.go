9f30232ef851f3c0ae546f805c5143cc4f841cb8
mrshubhambasu <54581984+mrshubhambasu@users.noreply.github.com>
2020-05-28 13:20:07 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	rice "github.com/GeertJohan/go.rice"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(rice.MustFindBox("dist").HTTPBox()))
+	http.ListenAndServe(":8080", nil)
+}
+
+// rice embed
+// go build
